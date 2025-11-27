c1eb9c66ed1b9b4e4bf87e6e1d04b416b3a0e186
지유진 <selena@jiyujin-ui-MacBookPro.local>
2025-11-27 17:39:54 +0900

    fix: emoji 순서 수정 및 빌드 업데이트

--- src/data/questions.js
+++ src/data/questions.js
@@ -3,111 +3,111 @@ export const questions = [
   {
     id: 11,
     question: "밤티 ",
+    emoji: "🤔",
     options: [
       { text: "연애 중인 사람을 부르는 애칭", answer: false },
       { text: "외모나 인상을 놀리며 '못생겼다'라는 의미", answer: true },
       { text: "밤에 주로 입는 티셔츠를 뜻하는 단어", answer: false },
       { text: "밤에 티한잔", answer: false }
-    ],
-    emoji: "🤔"
+    ]
   },
   {
     id: 12,
     question: "ㄷㄱㄹ ㄷㄱㅈ",
+    emoji: "🤔",
     options: [
       { text: "드가라 드가자", answer: false },
       { text: "다가를 들고자", answer: false },
       { text: "드가를 들가자", answer: false },
       { text: "드가를 드가자", answer: true }
-    ],
-    emoji: "�"
+    ]
   },
   {
     id: 13,
     question: "아자스",
+    emoji: "😎",
     options: [
       { text: "맛있는거 먹을때 추임새", answer: false },
       { text: "아저씨 부를때 애칭", answer: false },
       { text: "감사합니다의 의미", answer: true },
       { text: "피곤하다는 의미", answer: false }
-    ],
-    emoji: "�"
+    ]
   },
   {
     id: 14,
     question: "ㄷㅌㄴㅇ",
+    emoji: "🤔",
     options: [
       { text: "된탕나옴", answer: false },
       { text: "두통나욤", answer: false },
       { text: "도티낳음", answer: true },
       { text: "들통났음", answer: false }
-    ],
-    emoji: "🤔"
+    ]
   },
   {
     id: 15,
     question: "싹싹 김치",
+    emoji: "👍",
     options: [
       { text: "김치가 기쁜 마음에 박수를 치는 현상", answer: false },
       { text: "김치가 너무 짜서 두뇌가 초기화되는 순간", answer: false },
       { text: "너무맛있어서 싹싹 긁어먹음", answer: true },
       { text: "상대방한테 잘못해서 싹싹 비는 표현", answer: false }
-    ],
-    emoji: "�"
+    ]
   },
   {
     id: 16,
     question: "낋여오거라 표현중 맞는것은?",
+    emoji: "🤔",
     options: [
       { text: "남자친구좀 낋여오거라", answer: true },
       { text: "오늘 기분 좀 낋인다", answer: false },
       { text: "오늘 라면  좀 잘 낋여오네", answer: false },
       { text: "니얼굴 낋였다", answer: false }
-    ],
-    emoji: "🤔"
+    ]
   },
   {
     id: 17,
     question: "ㅇㅍㅌ 뜻",
+    emoji: "🤔",
     options: [
       { text: "영원한", answer: false },
       { text: "젊은 척하는 40대 / 트렌드 따라한다", answer: true },
       { text: "돈많은 40대 /  트렌드를 잘 따라간다", answer: false },
       { text: "오픈톡", answer: false }
-    ],
-    emoji: "🤔"
+    ]
   },
   {
     id: 18,
     question: "갑자기 대체용 ",
+    emoji: "🤔",
     options: [
       { text: "갑츠기스러운뎅", answer: false },
       { text: "갑짜기스르운데", answer: false },
       { text: "갑쳐기스러운데", answer: false },
       { text: "갑차기스러운데", answer: true }
-    ],
-    emoji: "🤔"
+    ]
   },
   {
     id: 19,
     question: "hmh과 비슷한 공식을 활용한 밈은?",
+    emoji: "🤔",
     options: [
       { text: "tmt", answer: false },
       { text: "dmd", answer: false },
       { text: "fmf", answer: false },
       { text: "gmg", answer: true }
-    ],
-    emoji: "🤔"
+    ]
   },
   {
     id: 20,
     question: "ㅅㅂ의 ",
+    emoji: "🤔",
     options: [
       { text: "샤갈", answer: true },
       { text: "상년", answer: false },
       { text: "시방", answer: false },
       { text: "시불", answer: false }
-    ],
-    emoji: "🤔"
+    ]
   }
 ];
